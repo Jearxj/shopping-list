42c1aae0c674512f8486927f0f2047fa8071f5cc
Jearranai Jujaroen <jear.jujaroen@gmail.com>
2014-04-07 23:22:04 -0400

    Fixed bug on input

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-    var is_text_in = [0];
     $('.text-board').on('keypress', function(event) {
         console.log("this is working");
         var item = $('input').val();
         if (event.which == 13) {
             console.log("enter key");
             event.preventDefault();
-            if (is_text_in.length === 1) {
+            if (item.length >= 1) {
                 console.log("true");
                 $('.items').append('<p>'+item+'</p>').css({"font-family":"Electrolize","color":"#4C4C4C"});
             } 
